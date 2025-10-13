2406687076216d3609bfcd27340701d397a79d7d
DrTellus <102365316+DrTellus@users.noreply.github.com>
2025-10-13 14:20:41 +0200

    Oppgave 3 ferdig

--- src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
@@ -2,14 +2,60 @@ package no.hvl.dat100.javel.oppgave3;
 
 public class Customer {
 
+    //a
+    private String name;
+    private String email;
+    private int customer_id;
+    private PowerAgreementType agreement;
+
     // TODO - object variables (attributes)
 
+
     public Customer(String name, String email, int customer_id, PowerAgreementType agreement) {
+        //b
+        this.name = name;
+        this.email = email;
+        this.customer_id = customer_id;
+        this.agreement = agreement;
+
 
         // TODO
+    //c
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public int getCustomer_id() {
+        return customer_id;
+    }
+    public PowerAgreementType getAgreement() {
+        return agreement;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setCustomer_id(int customer_id){
+        this.customer_id = customer_id;
+    }
+    public void setAgreement(PowerAgreementType agreement){
+        this.agreement = agreement;
     }
 
     // TODO - getter/setter methods for all objectvariables
+
+    public String toString() {
+        return  "Navn : " + name + "\n" +
+                "epost: " + email + "\n" +
+                "brukerid: " + customer_id + "\n" +
+                "avtaletype: " + agreement;
+    }
 
     // TODO - toString method
 
--- src/main/java/no/hvl/dat100/javel/oppgave3/CustomerMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/CustomerMain.java
@@ -13,9 +13,15 @@ public class CustomerMain {
 
         /*
         TODO
-
          Write code that creates a Customer object and teste the methods implemented in the class
-
         */
+        Customer kunde1 = new Customer("Adam", "adam@epost.no",243, PowerAgreementType.NORGESPRICE);
+        System.out.println(kunde1);
+
+        Customer kunde2 = new Customer("Kain", "kain@epost.no", 66, PowerAgreementType.SPOTPRICE);
+        System.out.println(kunde2);
+
+        Customer kunde3 = new Customer("Abel", "abel@epost.no", 02, PowerAgreementType.POWERSUPPORT);
+        System.out.println(kunde3);
     }
 }
